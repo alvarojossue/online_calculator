6e8e6dc81e431306883134770b9f48da954d3c6b
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-04-05 18:35:02 -0400

    Initial commit

---
+++ lib/calculator.rb
@@ -0,0 +1,18 @@
+class Calculator
+	def add(first_number, second_number)
+		result = first_number.to_f + second_number.to_f
+	end
+
+	def substract(first_number, second_number)
+		result = first_number.to_f - second_number.to_f
+	end
+
+	def multiply(first_number, second_number)
+		result = first_number.to_f * second_number.to_f
+	end
+
+	def divide(first_number, second_number)
+		result = first_number.to_f / second_number.to_f
+	end
+
+end
---
+++ server.rb
@@ -0,0 +1,55 @@
+require "sinatra"
+require_relative "lib/calculator.rb"
+
+get "/" do
+	erb(:home)
+end
+
+get "/add" do
+ 	erb(:add)
+end
+
+post "/calculate_add" do
+	number_1 = params[:first_number]
+	number_2 = params[:second_number]
+	my_addition = Calculator.new
+	@result = my_addition.add(number_1, number_2)
+	erb(:result_add)
+end
+
+get "/substract" do
+	erb(:substract)
+end
+
+post "/calculate_substract" do
+	number_1 = params[:first_number]
+	number_2 = params[:second_number]
+	my_substraction = Calculator.new
+	@result = my_substraction.substract(number_1, number_2)
+	erb(:result_substract)
+end
+
+get "/multiply" do
+	erb(:multiply)
+end
+
+post "/calculate_multiply" do
+	number_1 = params[:first_number]
+	number_2 = params[:second_number]
+	my_multiplication = Calculator.new
+	@result = my_multiplication.multiply(number_1, number_2)
+	erb(:result_multiply)
+end
+
+get "/divide" do
+	erb(:divide)
+end
+
+post "/calculate_divide" do
+	number_1 = params[:first_number]
+	number_2 = params[:second_number]
+	my_division = Calculator.new
+	@result = my_division.divide(number_1, number_2)
+	erb(:result_divide)
+end
+
